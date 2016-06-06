575be9a7dc31719a7ee6aff0f58600f5637cac9a
Upasna Pandey <upandey@tradestation.com>
2016-06-06 11:07:25 -0500

    Initial Commit

---
+++ SimulatorLauncher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SimulatorLauncher
+//
+//  Created by Upasna Pandey on 6/6/16.
+//  Copyright © 2016 TradeStation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
